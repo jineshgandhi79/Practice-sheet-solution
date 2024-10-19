1433f848bfbb92f60cf2b4ca0d66028175fa5d2b
Jinesh Gandhi <jineshgandhi79@gmail.com>
2024-10-19 23:58:53 +0530

    solution.js

--- solution.js
+++ solution.js
@@ -2,7 +2,7 @@
 // 1. Reverse Array
 function reverseArray(arr) {
     let reversed = [];
-    for (let i = arr.length - 1; i >= 0; i--) {
+    for (let i = arr.length - 1; i >= 0; i--) { 
         reversed.push(arr[i]);
     }
     return reversed;
